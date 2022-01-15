fb0a42720cac46f3f5e2886b63e55f0d79630769
dalcubierre <diegoalcubierre@hotmail.com>
2022-01-14 22:46:03 -0500

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Alcubierre Holdings Inc. on 2022-01-14.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
